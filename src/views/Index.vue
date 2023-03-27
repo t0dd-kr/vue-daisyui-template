7e016d4bdfdd1b7dad5ec0bfe154d40c660238cb
t0dd-kr <swjasontodd@gmail.com>
2023-03-27 15:44:36 +0900

    package: unplugin-auto-import 추가

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
-  import { useStore } from '@/stores'
-  import { storeToRefs } from 'pinia'
-  
+  // import { ref } from 'vue'
+  // import { useStore } from '@/stores'
+  // import { storeToRefs } from 'pinia'
+
   const store = useStore()
-  
+
   const { increment, decrement } = store
   const { count } = storeToRefs(store)
+
+  const hello = ref('')
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center fixed top-0 left-0 w-full h-full text-white">
-    <img src="@/assets/images/logo.png" alt="vue" class="w-40">
+  <div
+    class="flex flex-col items-center justify-center fixed top-0 left-0 w-full h-full text-white"
+  >
+    <img src="@/assets/images/logo.png" alt="vue" class="w-40" />
     <h1 class="mt-4 text-2xl">Hello world!</h1>
     <div class="mt-10 flex">
       <button class="btn text-lg" @click="decrement">-</button>
---
+++ vite.config.js
@@ -0,0 +1,69 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        'pinia',
+        // custom
+      ],
+      // Enable auto import by filename for default module exports under directories
+      defaultExportByFilename: false,
+
+      // Auto import for module exports under directories
+      // by default it only scan one level of modules under the directory
+      dirs: [
+        './src/**',
+        // './hooks',
+        // './composables' // only root modules
+        // './composables/**', // all nested modules
+        // ...
+      ],
+
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: './auto-imports.d.ts',
+
+      // Cache the result of resolving, across multiple vite builds.
+      // A custom path is supported.
+      // When set to `true`, the cache will be stored in `node_modules/.cache/unplugin-auto-import.json`.
+      cache: false,
+
+      // Auto import inside Vue template
+      // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
+      vueTemplate: false,
+
+      // Custom resolvers, compatible with `unplugin-vue-components`
+      // see https://github.com/antfu/unplugin-auto-import/pull/23/
+      resolvers: [
+        /* ... */
+      ],
+
+      // Inject the imports at the end of other imports
+      injectAtEnd: true,
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
