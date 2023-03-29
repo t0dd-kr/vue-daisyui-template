220b99b297c5002355a9bb36328945ac3165a9cd
t0dd-kr <swjasontodd@gmail.com>
2023-03-29 18:50:38 +0900

    pkg: unplugin-vue-components 추가

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Components({}),
     AutoImport({
       // targets to transform
       include: [
